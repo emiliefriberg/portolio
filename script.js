3ce7dc23305b14074b661bcd122fe2fe3b663999
emiliefriberg <44567474+emiliefriberg@users.noreply.github.com>
2019-01-08 10:27:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", sidenVises);
+
+
+function sidenVises() {
+    console.log("sidenVises)");
+}
+
+//egern
+
+document.querySelector("#egern_sprite").classList.add("blink");
